0f669e37449334498d85394f983c63d5f4e9e796
ramaiane_ms <ramaiane.ms@gmail.com>
2018-08-02 00:49:18 -0300

    criacao de projeto spring boot

---
+++ src/main/java/br/com/springbootstarter/hello/helloController.java
@@ -0,0 +1,15 @@
+package br.com.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+	
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "HI";
+		
+	}
+
+}
